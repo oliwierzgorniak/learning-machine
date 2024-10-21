4045eb6c1715a2090e25fe6e5ab30daba3a4eaa2
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2024-10-21 12:11:36 +0200

    swimping mechanism changed to a more reliable one

--- src/js/game/handleSwiping.js
+++ src/js/game/handleSwiping.js
@@ -3,8 +3,6 @@ import globals from "../globals";
 import { hands } from "../setup/setup";
 import getRelevantPoints from "./handleSwiping/getRelevantPoints";
 
-let lastHandPointX = null;
-let swipingDirection = null;
 let catchOffsetX = 0;
 let catchPointIndex = null;
 
@@ -23,10 +21,6 @@ const handleSwiping = () => {
             point.y < expression.position.y + EXPRESSION_HEIGHT &&
             point.y > expression.position.y
           ) {
-            swipingDirection =
-              point.x - expression.position.x < EXPRESSION_WIDTH / 2
-                ? "right"
-                : "left";
             catchOffsetX =
               point.x - expression.position.x < EXPRESSION_WIDTH / 2
                 ? 0
@@ -38,25 +32,10 @@ const handleSwiping = () => {
       });
     } else {
       const handPointX = relevantPoints[catchPointIndex].x;
-      if (lastHandPointX != null) {
-        if (
-          (swipingDirection == "left" && lastHandPointX < handPointX) ||
-          (swipingDirection == "right" && lastHandPointX > handPointX)
-        ) {
-          globals.caughtExpressionIndex = null;
-          swipingDirection = null;
-          catchOffsetX = 0;
-          catchPointIndex = null;
-        }
-      }
-
-      if (globals.caughtExpressionIndex != null) {
-        globals.expressions[globals.caughtExpressionIndex].position.x =
-          handPointX + catchOffsetX;
-        lastHandPointX = handPointX;
-      }
+      globals.expressions[globals.caughtExpressionIndex].position.x =
+        handPointX + catchOffsetX;
     }
-  }, 50);
+  }, 40);
 };
 
 export default handleSwiping;
--- src/js/ui/toggleMusic.js
+++ src/js/ui/toggleMusic.js
@@ -1,6 +1,5 @@
 const toggleMusic = () => {
   const $audio = document.querySelector(".playing-container__audio");
-  console.log($audio.paused);
   if ($audio.paused) {
     $audio.play();
   } else {
